81f56d5fde48911657aaaacf39a9b0f7289c3ee0
lopa-mudra-das <lopamudradas459@gmail.com>
2023-01-28 11:21:28 +0530

    added files for changing background color

---
+++ random.js
@@ -0,0 +1,10 @@
+const lopa=()=>{
+    
+    const randomNumber=Math.floor(Math.random()*16777215);
+    const randomCode="#" + randomNumber.toString(16);
+    document.body.style.backgroundColor=randomCode;
+    document.getElementById("color-code").innerText=randomCode;
+}
+document.getElementById("btn").addEventListener("click",lopa);
+
+// getColor();
